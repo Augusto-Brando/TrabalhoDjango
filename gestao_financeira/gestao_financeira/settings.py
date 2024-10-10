c9ad88667d009eb4b8d559256f7ca1172dbaf26d
augusto.brando <augusto.brando@datamint.com.br>
2024-10-10 12:34:05 -0300

    feat: criando area de admin

--- gestao_financeira/gestao_financeira/settings.py
+++ gestao_financeira/gestao_financeira/settings.py
@@ -1,9 +1,8 @@
-
-
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -72,7 +71,7 @@ DATABASES = {
 
         'NAME': 'gestao_financeira',
 
-        'USER': 'postgress',
+        'USER': 'postgres',
 
         'PASSWORD': 'root',
 
